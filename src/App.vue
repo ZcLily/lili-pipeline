6ada81378a23771d913e9d540ab98ca84517f168
caiwenzhan <caiwenzhan@bluemoon.com.cn>
2022-11-10 16:05:57 +0800

    fix style

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,19 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei',
+    '微软雅黑', Arial, sans-serif;
+}
+html body{
+  height: 100%;
+  width: 100%;
+  font-size: 14px;
+}
+ul li {
+  list-style:none;
 }
 </style>
